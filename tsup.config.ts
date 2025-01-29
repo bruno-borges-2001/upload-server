ee61f55bdb965f14db9c10200d515b5b735dd52e
Bruno Borges <brunoborges2001@gmail.com>
2025-01-29 18:29:42 -0300

    feat: docker setup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/**/*.ts'],
-  clean: true,
+  entry: ['src/**/*.ts', '!src/**/*.spec.ts', '!src/test/**/*.ts'],
   format: 'esm',
+  clean: true,
   outDir: 'dist',
 })
